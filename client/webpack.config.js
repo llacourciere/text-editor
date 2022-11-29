cd42d1f04899dc042ca98ee88dace2e6beac6dee
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-11-28 19:47:13 -0800

    added webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,11 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'JATE',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }),
     ],
 
