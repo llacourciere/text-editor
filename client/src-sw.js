42e06d9cc72d57124bad2ce83e52cd8c086dbfad
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-11-28 20:49:08 -0800

    button functionality

--- client/src-sw.js
+++ client/src-sw.js
@@ -30,7 +30,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 registerRoute(
   ({ request }) => request.destination === 'image',
   new CacheFirst({
-    cacheName: 'my-image-cache',
+    cacheName: 'my-assets-cache',
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
